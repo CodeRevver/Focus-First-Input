436ca247d65c5e696e7e181a2d062e06828a1997
Michael Clark <mjclark91@aol.com>
2018-05-20 19:55:08 +0100

    bit mroe functionality

--- src/inj.js
+++ src/inj.js
@@ -1,163 +1,206 @@
 // GLOBALS
-var keysPressed = [];
-var numericLinkOverlayEnabled = false;
-var numericLinkAttributeIdentifierPrefix = 'focus-first-input-numeric-link-number';
-var numericLinkClass = 'focus-first-input-numeric-link';
-var disableDonateNotification = false;
-var siteExclusionList = [];
-
-focusOnFirstInput = function () {
-  if (document.activeElement.tagName != "input") {
-    var inputs = document.body.getElementsByTagName("input")
-    focused = false
-    for (var i = 0; i < inputs.length; i++) {
-      var input = inputs[i]
-      if (elementInViewport(input) && (input.type == "text" || input.type == "search")) {
-        input.focus()
-        focused = true
-        break
-      }
-    }
-
-    if (!focused && inputs.length > 0)
-      for (var i = 0; i < inputs.length; i++) {
-        var input = inputs[i]
-        if (elementInViewport(input)) {
-          input.focus()
-          focused = true
-          break
-        }
-      }
-  }
+let appSettings = {
+  disableDonateNotification: false,
+  siteExclusionList: []
 }
 
-elementInViewport = function (el) {
-  var rect = el.getBoundingClientRect();
-
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
-
-enableNumericLinks = function () {
-  var i = 1;
-  var linkableElements = Array.from(document.getElementsByTagName("a"));
-  linkableElements.forEach(function (aLink) {
-    if (elementInViewport(aLink)) {
-
-      aLink.classList.add(numericLinkClass);
-      aLink.setAttribute(numericLinkAttributeIdentifierPrefix, i);
-      i++;
-      console.log('in vp');
+let helpers = {
+  logToConsole(message, logLevel = 'debug') {
+    if (this.isDevMode()) {
+      switch (logLevel) {
+        case 'error': console.error(message); break;
+        case 'warn': console.warn(message); break;
+        case 'info': console.info(message); break;
+        case 'debug': console.debug(message); break;
+        default: console.debug(message); break;
+      }
     }
-    else {
-      console.log('not in vp');
-    }
-  });
+  },
 
-  numericLinkOverlayEnabled = true;
-  console.log('numeric links activated');
-};
+  elementInViewport(el) {
+    let rect = el.getBoundingClientRect();
 
-disableNumericLinks = function () {
-  var i = 1;
-  var linkableElements = Array.from(document.getElementsByTagName("a"));
-  linkableElements.forEach(function (aLink) {
-    aLink.classList.remove(numericLinkClass);
-    aLink.removeAttribute(numericLinkAttributeIdentifierPrefix);
-    i++;
-  });
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  },
 
-  numericLinkOverlayEnabled = false;
-  console.log('numeric links deactivated');
-};
-
-clearKeysPressed = function () {
-  console.log('cleared keys pressed');
-  keysPressed = [];
-};
-
-toggleDonateNotification = function () {
-  var newDonateNotificationSetting = !disableDonateNotification;
-  chrome.storage.sync.set({ 'disableDonateNotification': newDonateNotificationSetting }, function () {
-    console.log('Donate notification set to: ' + newDonateNotificationSetting);
-  });
-};
-
-addToSiteExclusionList = function () {
-  var siteToAdd = window.location.host;
-  siteExclusionList.push(siteToAdd);
-  chrome.storage.sync.set({ 'siteExclusionList': siteExclusionList }, function () {
-    console.log('Site exclusion list set to: ' + siteExclusionList);
-  });
-};
-
-removeFromSiteExclusionList = function () {
-  var siteToRemove = window.location.host;
-  var positionOfItem = siteExclusionList.indexOf(siteToRemove);
-  if (positionOfItem !== -1) {
-    siteExclusionList.splice(positionOfItem, 1);
-    chrome.storage.sync.set({ 'siteExclusionList': siteExclusionList }, function () {
-      console.log('Site exclusion list set to: ' + siteExclusionList);
-    });
+  isDevMode() {
+    return !('update_url' in chrome.runtime.getManifest());
   }
 };
 
-init = function () {
-  chrome.storage.sync.get(['disableDonateNotification', 'siteExclusionList'], function (result) {
-    disableDonateNotification = result.disableDonateNotification ? result.disableDonateNotification : false;
-    siteExclusionList = result.siteExclusionList ? result.siteExclusionList : [];
-    console.log('Settings retrieved', result);
-  });
+class ScreenOverlord {
+  constructor() {
+    this.keysPressed = [];
+    this.numericLinkOverlayEnabled = false;
+  }
 
-  // Setup event listeners
-  document.onkeyup = function (e) {
-    if (e.keyCode === 18 && numericLinkOverlayEnabled) {
-      console.log('released alt key');
+  onkeyup(e) {
+    if (e.keyCode === 18 && this.numericLinkOverlayEnabled) {
+      helpers.logToConsole('released alt key');
       e.preventDefault();
 
-      if (keysPressed.length > 0) {
+      if (this.keysPressed.length > 0) {
         //// Has some numbers
-        var numericValueOfLink = keysPressed.join('');
-        console.log('keys pressed: alt+' + numericValueOfLink);
-        var elementToClick = document.querySelector('[' + numericLinkAttributeIdentifierPrefix + '="' + numericValueOfLink + '"]');
+        let numericValueOfLink = this.keysPressed.join('');
+        helpers.logToConsole('keys pressed: alt+' + numericValueOfLink);
+        let elementToClick = document.querySelector('[focus-first-input-numeric-link-number="' + numericValueOfLink + '"]');
         if (elementToClick) {
           elementToClick.click();
         }
       }
 
-      disableNumericLinks();
-      clearKeysPressed();
+      this.disableNumericLinks();
+      this.clearKeysPressed();
     }
 
-    var isAltS = keysPressed.length === 1 && keysPressed[0] === 's';
+    let isAltS = this.keysPressed.length === 1 && this.keysPressed[0] === 's';
 
     if (isAltS) {
       e.preventDefault();
-      focusOnFirstInput();
-      clearKeysPressed();
+      this.focusOnFirstInput();
+      this.clearKeysPressed();
       return;
     }
+
+    helpers.logToConsole('Event listeners set up');
   }
 
-  document.onkeydown = function (e) {
-    if (e.altKey && !numericLinkOverlayEnabled) {
+  onkeydown(e) {
+    if (e.altKey && !this.numericLinkOverlayEnabled) {
       // Alt key pressed by itself
       e.preventDefault();
-      enableNumericLinks();
+      this.enableNumericLinks();
     }
 
-    var keyCodeBetween0To9 = e.key >= 0 && e.key <= 9;
-    var keyCodeS = e.keyCode === 83;
+    let keyCodeBetween0To9 = e.key >= 0 && e.key <= 9;
+    let keyCodeS = e.keyCode === 83;
     if (e.altKey && (keyCodeBetween0To9 || keyCodeS)) {
       e.preventDefault();
-      keysPressed.push(e.key);
-      console.log('pushed ' + e.key);
+      this.keysPressed.push(e.key);
+      helpers.logToConsole('pushed ' + e.key);
     }
   }
+
+  onblur(e) {
+    this.disableNumericLinks();
+    this.clearKeysPressed();
+  }
+
+  focusOnFirstInput() {
+    if (document.activeElement.tagName != "input") {
+      let inputs = document.body.getElementsByTagName("input")
+      let focused = false
+      for (let i = 0; i < inputs.length; i++) {
+        let input = inputs[i]
+        if (helpers.elementInViewport(input) && (input.type == "text" || input.type == "search")) {
+          input.focus()
+          focused = true
+          break
+        }
+      }
+
+      if (!focused && inputs.length > 0)
+        for (let i = 0; i < inputs.length; i++) {
+          let input = inputs[i]
+          if (helpers.elementInViewport(input)) {
+            input.focus()
+            focused = true
+            break
+          }
+        }
+    }
+  }
+
+  enableNumericLinks() {
+    let i = 1;
+    let linkableElements = Array.from(document.getElementsByTagName("a"));
+    linkableElements.forEach(function (aLink) {
+      if (helpers.elementInViewport(aLink)) {
+        aLink.classList.add('focus-first-input-numeric-link');
+        aLink.setAttribute('focus-first-input-numeric-link-number', i);
+        i++;
+      }
+    });
+
+    this.numericLinkOverlayEnabled = true;
+    helpers.logToConsole('numeric links activated');
+  };
+
+  disableNumericLinks() {
+    let i = 1;
+    let linkableElements = Array.from(document.getElementsByTagName("a"));
+    linkableElements.forEach(function (aLink) {
+      aLink.classList.remove('focus-first-input-numeric-link');
+      aLink.removeAttribute('focus-first-input-numeric-link-number');
+      i++;
+    });
+
+    this.numericLinkOverlayEnabled = false;
+    helpers.logToConsole('numeric links deactivated');
+  };
+
+  toggleDonateNotification() {
+    let newDonateNotificationSetting = !appSettings.disableDonateNotification;
+    chrome.storage.sync.set({ 'disableDonateNotification': newDonateNotificationSetting }, function () {
+      helpers.logToConsole('Donate notification set to: ' + newDonateNotificationSetting);
+    });
+  };
+
+  addToSiteExclusionList() {
+    let siteToAdd = window.location.host;
+    appSettings.siteExclusionList.push(siteToAdd);
+    chrome.storage.sync.set({ 'siteExclusionList': appSettings.siteExclusionList }, function () {
+      helpers.logToConsole('Site exclusion list set to: ' + appSettings.siteExclusionList);
+    });
+  };
+
+  removeFromSiteExclusionList() {
+    let siteToRemove = window.location.host;
+    let positionOfItem = appSettings.siteExclusionList.indexOf(siteToRemove);
+    if (positionOfItem !== -1) {
+      appSettings.siteExclusionList.splice(positionOfItem, 1);
+      chrome.storage.sync.set({ 'siteExclusionList': appSettings.siteExclusionList }, function () {
+        helpers.logToConsole('Site exclusion list set to: ' + appSettings.siteExclusionList);
+      });
+    }
+  };
+
+  clearKeysPressed() {
+    this.keysPressed = [];
+    helpers.logToConsole('cleared keys pressed');
+  };
+};
+
+init = function () {
+  chrome.storage.sync.get(['disableDonateNotification', 'siteExclusionList'], function (result) {
+    appSettings.disableDonateNotification = result.disableDonateNotification ? result.disableDonateNotification : false;
+    appSettings.siteExclusionList = result.siteExclusionList ? result.siteExclusionList : ['www.google.co.uk', 'www.google.com'];
+    helpers.logToConsole('Settings retrieved', result);
+
+    let siteIsExcluded = appSettings.siteExclusionList.includes(window.location.host);
+    if (!siteIsExcluded) {
+      var screenOverlord = new ScreenOverlord();
+
+      document.onkeyup = function (e) {
+        screenOverlord.onkeyup(e);
+      }
+
+      document.onkeydown = function (e) {
+        screenOverlord.onkeydown(e);
+      }
+
+      window.onblur = function (e) {
+        screenOverlord.onblur(e);
+      }
+    } else {
+      helpers.logToConsole('Site is exluded - functionality disabled');
+    }
+  });
 };
 
 init();
