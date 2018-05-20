bd47dba61187bcad3c1d94e38217d4aba09a4e31
Michael Clark <mjclark91@aol.com>
2018-05-20 15:26:42 +0100

    more shizzzle in this nizzle

--- src/inj.js
+++ src/inj.js
@@ -3,6 +3,8 @@ var keysPressed = [];
 var numericLinkOverlayEnabled = false;
 var numericLinkAttributeIdentifierPrefix = 'focus-first-input-numeric-link-number';
 var numericLinkClass = 'focus-first-input-numeric-link';
+var disableDonateNotification = false;
+var siteExclusionList = [];
 
 focusOnFirstInput = function () {
   if (document.activeElement.tagName != "input") {
@@ -30,22 +32,13 @@ focusOnFirstInput = function () {
 }
 
 elementInViewport = function (el) {
-  var top = el.offsetTop;
-  var left = el.offsetLeft;
-  var width = el.offsetWidth;
-  var height = el.offsetHeight;
-
-  while (el.offsetParent) {
-    el = el.offsetParent;
-    top += el.offsetTop;
-    left += el.offsetLeft;
-  }
+  var rect = el.getBoundingClientRect();
 
   return (
-    top >= window.pageYOffset &&
-    left >= window.pageXOffset &&
-    (top + height) <= (window.pageYOffset + window.innerHeight) &&
-    (left + width) <= (window.pageXOffset + window.innerWidth)
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 };
 
@@ -53,11 +46,18 @@ enableNumericLinks = function () {
   var i = 1;
   var linkableElements = Array.from(document.getElementsByTagName("a"));
   linkableElements.forEach(function (aLink) {
-    aLink.classList.add(numericLinkClass);
-    aLink.setAttribute(numericLinkAttributeIdentifierPrefix, i);
-    i++;
+    if (elementInViewport(aLink)) {
+
+      aLink.classList.add(numericLinkClass);
+      aLink.setAttribute(numericLinkAttributeIdentifierPrefix, i);
+      i++;
+      console.log('in vp');
+    }
+    else {
+      console.log('not in vp');
+    }
   });
-  
+
   numericLinkOverlayEnabled = true;
   console.log('numeric links activated');
 };
@@ -75,53 +75,89 @@ disableNumericLinks = function () {
   console.log('numeric links deactivated');
 };
 
-clearKeysPressed = function() {
+clearKeysPressed = function () {
   console.log('cleared keys pressed');
   keysPressed = [];
 };
 
-document.onkeyup = function (e) {
-  if (e.keyCode === 18 && numericLinkOverlayEnabled) {
-    console.log('released alt key');
-    e.preventDefault();
-    // Alt key is released
+toggleDonateNotification = function () {
+  var newDonateNotificationSetting = !disableDonateNotification;
+  chrome.storage.sync.set({ 'disableDonateNotification': newDonateNotificationSetting }, function () {
+    console.log('Donate notification set to: ' + newDonateNotificationSetting);
+  });
+};
 
-    if (keysPressed.length > 0) {
-      //// Has some numbers
-      var numericValueOfLink = keysPressed.join('');
-      console.log('keys pressed: alt+' + numericValueOfLink);
-      var elementToClick = document.querySelector('[' + numericLinkAttributeIdentifierPrefix + '="' + numericValueOfLink + '"]');
-      if(elementToClick){
-        elementToClick.click();
+addToSiteExclusionList = function () {
+  var siteToAdd = window.location.host;
+  siteExclusionList.push(siteToAdd);
+  chrome.storage.sync.set({ 'siteExclusionList': siteExclusionList }, function () {
+    console.log('Site exclusion list set to: ' + siteExclusionList);
+  });
+};
+
+removeFromSiteExclusionList = function () {
+  var siteToRemove = window.location.host;
+  var positionOfItem = siteExclusionList.indexOf(siteToRemove);
+  if (positionOfItem !== -1) {
+    siteExclusionList.splice(positionOfItem, 1);
+    chrome.storage.sync.set({ 'siteExclusionList': siteExclusionList }, function () {
+      console.log('Site exclusion list set to: ' + siteExclusionList);
+    });
+  }
+};
+
+init = function () {
+  chrome.storage.sync.get(['disableDonateNotification', 'siteExclusionList'], function (result) {
+    disableDonateNotification = result.disableDonateNotification ? result.disableDonateNotification : false;
+    siteExclusionList = result.siteExclusionList ? result.siteExclusionList : [];
+    console.log('Settings retrieved', result);
+  });
+
+  // Setup event listeners
+  document.onkeyup = function (e) {
+    if (e.keyCode === 18 && numericLinkOverlayEnabled) {
+      console.log('released alt key');
+      e.preventDefault();
+
+      if (keysPressed.length > 0) {
+        //// Has some numbers
+        var numericValueOfLink = keysPressed.join('');
+        console.log('keys pressed: alt+' + numericValueOfLink);
+        var elementToClick = document.querySelector('[' + numericLinkAttributeIdentifierPrefix + '="' + numericValueOfLink + '"]');
+        if (elementToClick) {
+          elementToClick.click();
+        }
       }
+
+      disableNumericLinks();
+      clearKeysPressed();
     }
 
-    disableNumericLinks();
-    clearKeysPressed();
+    var isAltS = keysPressed.length === 1 && keysPressed[0] === 's';
+
+    if (isAltS) {
+      e.preventDefault();
+      focusOnFirstInput();
+      clearKeysPressed();
+      return;
+    }
   }
 
-  var isAltS = keysPressed.length === 1 && keysPressed[0] === 83;
+  document.onkeydown = function (e) {
+    if (e.altKey && !numericLinkOverlayEnabled) {
+      // Alt key pressed by itself
+      e.preventDefault();
+      enableNumericLinks();
+    }
 
-  if (isAltS) {
-    e.preventDefault();
-    focusOnFirstInput();
-    clearKeysPressed();
-    return;
+    var keyCodeBetween0To9 = e.key >= 0 && e.key <= 9;
+    var keyCodeS = e.keyCode === 83;
+    if (e.altKey && (keyCodeBetween0To9 || keyCodeS)) {
+      e.preventDefault();
+      keysPressed.push(e.key);
+      console.log('pushed ' + e.key);
+    }
   }
-}
+};
 
-document.onkeydown = function (e) {
-  if (e.altKey && !numericLinkOverlayEnabled) {
-    e.preventDefault();
-    // Alt key pressed by itself
-    enableNumericLinks();
-  }
-
-  var keyCodeBetween0To9 = e.key >= 0 && e.key <= 9;
-  var keyCodeS = e.keyCode === 83;
-  if (e.altKey && (keyCodeBetween0To9 || keyCodeS)) {
-    e.preventDefault();
-    keysPressed.push(e.key);
-    console.log('pushed ' + e.key);
-  }
-}
+init();
